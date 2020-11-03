2b5b7d333b3ed011ea35b769aeec5cbf54e362c3
sumin_030829 <sumin0829@kako.com>
2020-11-03 20:07:54 +0900

    complete viewflippertest01

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimviewcontainar/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimviewcontainar/MainActivity.java
@@ -3,12 +3,34 @@ package kr.hs.emirim.s2019s38.mirimviewcontainar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drawer);
+        flipper = findViewById(R.id.flipper);
+        Button btnPrev=findViewById(R.id.btn_prev);
+        Button btnNext=findViewById(R.id.btn_next);
+        btnPrev.setOnClickListener(btnListener);
+        btnNext.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.btn_prev:
+                    flipper.showPrevious();
+                    break;
+                case R.id.btn_next:
+                    flipper.showNext();
+                    break;
+            }
+        }
+    };
 }
