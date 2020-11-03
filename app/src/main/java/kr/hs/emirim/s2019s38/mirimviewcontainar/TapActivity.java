4beb5e57e5ae7776fb3893b6ede999e5ab612c9c
sumin_030829 <sumin0829@kako.com>
2020-11-03 20:40:07 +0900

    complete viewflippertest01

---
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimviewcontainar/TapActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s38.mirimviewcontainar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap);
+    }
+}
