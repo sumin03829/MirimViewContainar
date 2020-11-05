fc6ad04fcff77dcc3a3e2537a1e82310a968c705
sumin_030829 <sumin0829@kako.com>
2020-11-05 20:26:40 +0900

    complete viewflippertest01

---
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimviewcontainar/tabSctivityTEst_2.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s38.mirimviewcontainar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tabSctivityTEst_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_sctivity_t_est_2);
+    }
+}
